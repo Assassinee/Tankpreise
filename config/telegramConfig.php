76b63595504d1b76dffd40abd50d1980bb1b5b23
Max <maxkosowski96@gmail.com>
2020-10-12 13:43:22 +0200

    Update telegramConfig.php

--- config/telegramConfig.php
+++ config/telegramConfig.php
@@ -3,6 +3,13 @@
 $telegramConfig = [
     'xmlfile' => __DIR__ . '/telegram.xml',
     'token' => '',
+    'dailyinfo' => [
+        'time' => 28800,//8Uhr
+        'type' => 'E5',
+        'users' => [
+            'User1' => 'XXXXXXXXX'
+        ]
+    ],
     'users' => [
         'User1' => 'XXXXXXXXX'
     ]
